88ad512a29e266bed2429e22bde9df511689f170
ribrciti <rbciti@msn.com>
2014-12-10 10:23:19 -0600

    Added test data to fixtures, with associations and validations to models

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ActiveRecord::Base
 	has_many :users
 	has_many :projects
+
+	validates :name, length: { minimum: 5 ) }
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
 	belongs_to :company
 	has_many :works
 	has_many :projects, :through => :works
+
+	valideates: :fname, length: { minimum: 2}
+	valideates: :lname, length: { minimum: 5}
+	valideates: :company, presence: true 
+
 end
 
